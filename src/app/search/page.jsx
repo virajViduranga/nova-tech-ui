248250df5f3bf37be867c446eb8056af2d59b88d
Viraj Viduranga <163404822+virajViduranga@users.noreply.github.com>
2026-07-30 09:23:15 +0530

    UI Updates

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,13 +1,29 @@
 import FilterSidebar from "../../../components/FilterSidebar";
 import Navbar from "../../../components/Navbar";
+import Cards from "../../../components/Cards";
+import Footer from "../../../components/Footer";
 
-export default function SearchPage(){
-    return(
-        <>
-      <div className="max-w-full">
-          <Navbar/>
-        <FilterSidebar />
+export default function SearchPage() {
+  return (
+    <>
+      <div className="max-w-full min-h-screen flex flex-col">
+        <Navbar />
+        <div className="flex flex-1 w-full">
+          <FilterSidebar />
+
+          {/* Added 'w-full' so this container takes up the rest of the empty black space */}
+          <div className="flex flex-col bg-amber-50 w-full p-6">
+            <h1 className="text-black text-2xl font-bold mb-4">
+              Search Results
+            </h1>
+
+            <div className="flex">
+              <Cards />
+            </div>
+          </div>
+        </div>
+    <Footer/>
       </div>
-        </>
-    );
+    </>
+  );
 }
